e38fc819c5d4db0e9d57d7b865b29cab3809b3ed
JEdmilson <jedpraia@gmail.com>
2023-04-12 22:50:41 -0300

    Chess layer and prit board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,17 @@
 package application;
 
 import boardgame.Board;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
+		//by ED
 		// TODO Auto-generated method stub
 		//System.out.println("Hello ");
-		Board board=new Board(8, 8);
+		ChessMatch chessMatch=new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
+		
 
 	}
 
--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -27,6 +27,13 @@ public class Board {
 	public void setColumns(int columns) {
 		this.columns = columns;
 	}
+	public Piece piece (int row, int col) {
+		return pieces[row][col];
+	}
+	//sobrecarga de método:
+	public Piece piece (Position position) {
+		return pieces[position.getRow()][position.getCol()];
+	}
 	
 	
 	
